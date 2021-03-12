6520ad54fcef3b804e5a40b266822614765be3d4
Chantz-J <johnson.chantz@gmail.com>
2021-03-12 16:07:03 -0500

    custom hook working!

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
+import React, { useState } from "react" 
+import { useForm } from '../hooks/useForm'
 
-const initialValue = {
-  firstName: "",
-  lastName: "",
-  address: "",
-  city: "",
-  state: "",
-  zip: "",
-};
+// const initialValue = {
+//   firstName: "",
+//   lastName: "",
+//   address: "",
+//   city: "",
+//   state: "",
+//   zip: "",
+// };
 
 // This form should be handled by a "useForm" custom hook
 // Build out the logic needed for a form custom hook (see the useForm.js file)
@@ -15,56 +16,73 @@ const initialValue = {
 
 const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
+  // const [values, setValues] = useState(initialValue);
 
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+  // const handleChanges = (e) => {
+  //   setValues({ ...values, [e.target.name]: e.target.value });
+  // };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setShowSuccessMessage(true);
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   setShowSuccessMessage(true);
+  // };
+  const [firstName, setFirstName, handleFirstName] = useForm("")
+  const [lastName, setLastName, handleLastName] = useForm("")
+  const [address, setAddress, handleAddress] = useForm("")
+  const [city, setCity, handleCity] = useForm("")
+  const [state, setState, handleState] = useForm("")
+  const [zipcode, setZipcode, handleZipcode] = useForm("")
+
+  const resetValues = e => {
+    e.preventDefault()
+    setShowSuccessMessage(true)
+    setFirstName("")
+    setLastName("")
+    setAddress("")
+    setCity("")
+    setState("")
+    setZipcode("")
+  }
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={resetValues}>
         <h2>Checkout Form</h2>
         <label>
           First Name:
           <input
             name="firstName"
-            value={values.firstName}
-            onChange={handleChanges}
+            value={firstName}
+            onChange={e => handleFirstName(e.target.value)}
           />
         </label>
         <label>
           Last Name:
           <input
             name="lastName"
-            value={values.lastName}
-            onChange={handleChanges}
+            value={lastName}
+            onChange={e => handleLastName(e.target.value)}
           />
         </label>
         <label>
           Address:
           <input
             name="address"
-            value={values.address}
-            onChange={handleChanges}
+            value={address}
+            onChange={e => handleAddress(e.target.value)}
           />
         </label>
         <label>
           City:
-          <input name="city" value={values.city} onChange={handleChanges} />
+          <input name="city" value={city} onChange={e => handleCity(e.target.value)} />
         </label>
         <label>
           State:
-          <input name="state" value={values.state} onChange={handleChanges} />
+          <input name="state" value={state} onChange={e => handleState(e.target.value)} />
         </label>
         <label>
           Zip:
-          <input name="zip" value={values.zip} onChange={handleChanges} />
+          <input name="zipcode" value={zipcode} onChange={e => handleZipcode(e.target.value)} />
         </label>
         <button>Checkout</button>
       </form>
@@ -78,11 +96,11 @@ const CheckoutForm = (props) => {
           <br />
           <br />
           <p>
-            {values.firstName} {values.lastName}
+            {firstName} {lastName}
           </p>
-          <p>{values.address}</p>
+          <p>{address}</p>
           <p>
-            {values.city}, {values.state} {values.zip}
+            {city}, {state} {zipcode}
           </p>
         </div>
       )}
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -33,7 +33,7 @@ export default class PlantList extends Component {
   render() {
     return (
       <main className="plant-list">
-        {this.state?.plants?.map((plant) => (
+        {this.state.loading ? <h1>Loading...</h1> : this.state?.plants?.map((plant) => (
           <div className="plant-card" key={plant.id} data-testid="plant-card">
             <img className="plant-image" src={plant.img} alt={plant.name} />
             <div className="plant-details">
